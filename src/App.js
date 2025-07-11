21fc5c1a3f6e71add5070c0685c34bff086d2029
majda-k <kamrain.majda@gmail.com>
2025-07-11 22:00:44 +0100

    app meteo tanger

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CardMeteo from "./cardMeteo";
+import { Container } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+import "moment/locale/ar"; // Import Arabic locale for moment.js
+import i18n from "./i18n"; // Import your i18n instance
+
+const theme = createTheme({
+  topography: {
+    fontFamily: "IBM",
+    fontSize: 14,
+    fontWeight: 500,
+  },
+});
 
 function App() {
+
+  const [dateAndTime, setDateAndTime] = useState("")
+  const cancelAxiosRef = React.useRef(null);
+  const [temp, setTemp] = useState({
+    temp: null,
+    min: null,
+    max: null,
+    description: null,
+    icon: null,
+  });
+  useEffect(() => {
+    i18n.changeLanguage("ar");
+    setDateAndTime(moment().locale("ar").format("MMMM Do YYYY, h:mm:ss a"));
+    axios({
+      cancelToken: new axios.CancelToken((c) => {
+        cancelAxiosRef.current = c;
+      }),
+      method: "get",
+      url: "https://api.openweathermap.org/data/2.5/weather?lat=35.7696302&lon=-5.8033522&appid=e125df6838fd4205cab05e0d660fbd41&units=metric",
+    })
+      
+    
+    
+    .then(function (response) {
+        // handle success
+        const temp = Math.round(response.data.main.temp);
+        console.log(temp);
+
+        const min = Math.round(response.data.main.temp_min);
+        const max = Math.round(response.data.main.temp_max);
+        const description = response.data.weather[0].description;
+        const icon = response.data.weather[0].icon;
+        const dateAndTime = moment().locale("ar").format("MMMM Do YYYY, h:mm:ss a");
+        console.log(response.data);
+
+        setTemp({
+          temp: temp,
+          min: min,
+          max: max,
+          description: description,
+          icon:`https://openweathermap.org/img/wn/${icon}@2x.png`,
+          dateAndTime: dateAndTime,
+        });
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+    return () => {
+      // cleanup function to cancel the request if the component unmounts
+      console.log("Cleaning up...");
+
+      cancelAxiosRef.current();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <div className="App">
+        <ThemeProvider theme={theme}>
+          <div
+            style={{
+              backgroundColor: "#4169E1",
+              height: "100vh",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Container maxWidth="sm">
+              <CardMeteo temp={temp} />
+            </Container>
+          </div>
+        </ThemeProvider>
+      </div>
+    </React.StrictMode>
   );
 }
 
---
+++ src/cardMeteo.jsx
@@ -0,0 +1,134 @@
+import CardContent from "@mui/material/CardContent";
+import { Card } from "@mui/material";
+import CloudIcon from "@mui/icons-material/Cloud";
+import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
+import "moment/locale/ar"; // Arabe
+import "moment/locale/en-gb"; // Anglais
+import moment from "moment";
+import "moment/locale/ar"; // Import Arabic locale for moment.js
+
+export default function CardMeteo({ temp }) {
+  const { t, i18n } = useTranslation();
+  const [locale, setLocale] = useState("ar");
+  const [dateAndTime, setDateAndTime] = useState("");
+
+  useEffect(() => {
+    // Définit la langue initiale à l’arabe
+    i18n.changeLanguage("ar");
+    moment.locale("ar");
+    setDateAndTime(moment().format("dddd, D MMMM YYYY - h:mm:ss A"));
+  }, []);
+
+  const handleLanguageClick = () => {
+    if (locale === "ar") {
+      setLocale("en");
+      i18n.changeLanguage("en-gb");
+      moment.locale("en");
+    } else {
+      setLocale("ar");
+      i18n.changeLanguage("ar");
+      moment.locale("ar");
+    }
+    setDateAndTime(moment().format("dddd, D MMMM YYYY - h:mm:ss A"));
+  };
+
+  return (
+    <>
+      <Card
+        style={{ padding: "10px", marginTop: "20px" }}
+        const
+        direction={locale === "ar" ? "rtl" : "ltr"}
+        sx={{
+          minWidth: 275,
+          backgroundColor: "#0000CD",
+          borderRadius: 2,
+          marginTop: 0,
+          color: "white",
+          maxHeight: 310,
+        }}
+      >
+        <CardContent style={{ padding: "0" }}>
+          <span
+            style={{
+              fontSize: 50,
+              fontFamily: "IBM",
+            }}
+          >
+            {t("Tanger")}{" "}
+          </span>{" "}
+          <span
+            style={{
+              fontSize: "18",
+              fontWeight: "normal",
+              marginRight: "20px",
+            }}
+          >
+            {dateAndTime}
+          </span>
+        </CardContent>
+        <hr />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>
+            <CloudIcon style={{ fontSize: "200" }} />
+          </div>
+          <div style={{ textAlign: "end" }}>
+            <div style={{ display: "flex" }}>
+              <div>
+                {temp.icon && (
+                  <img
+                    src={temp.icon}
+                    alt=""
+                    style={{
+                      fontSize: "20",
+                      marginTop: "30",
+                      marginLeft: "30px",
+                    }}
+                  />
+                )}
+              </div>
+              <CardContent sx={{ fontSize: 50, fontFamily: "IBM" }}>
+                {temp.temp}°C
+              </CardContent>
+            </div>
+            <CardContent sx={{ fontSize: 16, fontFamily: "IBM" }}>
+              {" "}
+              {t(temp.description)}{" "}
+            </CardContent>
+            <CardContent sx={{ fontSize: 10, fontFamily: "IBM" }}>
+              {" "}
+              <span>
+                {" "}
+                {temp.min} : {t("min")} :{" "}
+              </span>{" "}
+              <span>
+                {" "}
+                {temp.max} : {t("max")}{" "}
+              </span>
+            </CardContent>
+          </div>
+        </div>
+      </Card>
+
+      <button
+        style={{
+          textAlign: "end",
+          color: "white",
+          backgroundColor: "rgb(65, 105, 225)",
+          border: "none",
+          cursor: "pointer",
+          marginTop: "10px",
+          fontSize: "16px",
+        }}
+        onClick={handleLanguageClick}
+      >
+        {locale === "ar" ? "العربية" : "English"}
+      </button>
+    </>
+  );
+}
